e29229e80db5980a4a133e5d7ca549dd0e882222
Jay-AHR <Jay-AHR@users.noreply.github.com>
2022-02-28 18:56:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and ... uh ... save to reload, ya filthy animal!. I know a thing or two about reloadin' pardner!
         </p>
         <a
           className="App-link"
